7b6cd04f05929f86040b6180aaf29ef4b885146a
Umakant_kunal <Kunalumakant07@gmail.com>
2023-12-17 10:13:07 +0530

    update1
    updated whole website.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Function to toggle dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    const nav = document.querySelector('nav');
+
+    // Toggle dark mode by toggling CSS classes
+    body.classList.toggle('dark-mode');
+    nav.classList.toggle('dark-mode-nav');
+}
+
+
+
+
+
+  
